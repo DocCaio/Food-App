77ac80edeb657751499b60a23ba6a82adfc77324
Caio <108128119+DocCaio@users.noreply.github.com>
2023-08-14 16:46:44 -0300

    up

--- assets/app/menu.js
+++ assets/app/menu.js
@@ -3,7 +3,7 @@ const button = document.querySelector('#menu__button')
 button.addEventListener('click', funcaoCallback)
 
 function funcaoCallback() {
-  const menuNav = document.querySelector('.div-nav-links')
+  const menuNav = document.querySelector('.div-nav-link')
   menuNav.classList.toggle('nav-active')
 
   button.textContent === "Abrir"
